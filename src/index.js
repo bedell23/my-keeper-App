709ef0248f208beaab8078abc37b6c23308d4161
Doloquee Bedell <doloqueebedell71@gmail.com>
2020-09-23 22:22:59 +0000

    jsx practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentDate = new Date();
+const currentTime = currentDate.getHours();
+
+let greeting;
+
+let customStyle = {
+  color: "",
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}{" "}
+    <h1 className="heading" style={customStyle}>{`${greeting}`}</h1>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
