e093753f073535fb73e5118ba9beeaf1349fdbe4
Doloquee Bedell <doloqueebedell71@gmail.com>
2020-10-07 21:41:55 +0000

    Stage three

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {/* <Note /> */}
-      {notes.map((props) => (
-        <Note Key={props.keys} title={props.title} content={props.content} />
-      ))}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
